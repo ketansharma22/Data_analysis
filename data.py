e55b6203f7c29e6b83b4b9b6ff08ade48b68cb57
ketansharma22 <kanu220504@gmail.com>
2024-07-15 22:41:35 +0530

    some progress

--- data.py
+++ data.py
@@ -9,7 +9,11 @@ df=pd.read_csv('Data.csv',encoding='unicode_escape')
 #drop blank/unused columns
 df.drop(['Status','unnamed1'],axis=1,inplace=True)
 
+# droping null values
 df.dropna(inplace=True)
 
+# renaming of columns(optional)
 df.rename(columns={'Marital_Status':'Shadi'},inplace=True)
 print(df.info())
+
+#Exploratory Data analysis
